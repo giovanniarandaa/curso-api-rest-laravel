fb948132b74e0185204f52d2fb4f189b4a5cfb54
Giovanni Aranda <giovanni.arandaa@gmail.com>
2021-04-11 14:36:08 -0500

    Finalizacion curso

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
-    ->only(['show', 'index', 'destroy']);
+// V1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['show', 'index', 'destroy'])
+    ->middleware('auth:sanctum');
+
+
+// V2
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['show', 'index', 'destroy'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class, 'login']);
